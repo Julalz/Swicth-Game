e2b36b8caf2d96a55c9e6ee42798abf84e01d42c
julianalz <julian942@hotmail.com>
2023-03-25 16:50:30 +0100

    text animacion, li con colores, footer

--- javaScr/main.js
+++ javaScr/main.js
@@ -16,14 +16,13 @@ function colorRGB() {
   return "RGB" + coolor;
 }
 
-console.log(colorRGB());
 //creamos header dentro de main
 const secondHeader = document.createElement("header");
 const main = document.querySelector("main");
 const bigheader = document.querySelector("header");
 main.appendChild(secondHeader);
 
-secondHeader.textContent = colorRGB();
+const colorParaAdivinar = (secondHeader.textContent = colorRGB());
 
 //creamos main dentro de main
 
@@ -32,10 +31,10 @@ main.append(secondMain);
 //clases
 secondMain.classList.add("cards");
 main.classList.add("principal");
-secondHeader.classList.add("RGBfixed");
+secondHeader.classList.add("colorfixed");
 bigheader.classList.add("header1");
 
-//ul
+//lis con ul
 
 const li1 = document.createElement("li");
 const li2 = document.createElement("li");
@@ -44,6 +43,27 @@ const li4 = document.createElement("li");
 const li5 = document.createElement("li");
 const li6 = document.createElement("li");
 
+//damos id a cada uno del id con su respectivo color
+li1.classList.add("firstcard");
+li2.classList.add("secondcard");
+li3.classList.add("thirdcard");
+li4.classList.add("fourthcard");
+li5.classList.add("fifthcard");
+li6.classList.add("sixthcard");
+
+li1.style.backgroundColor = colorParaAdivinar;
+li2.style.backgroundColor = colorRGB();
+li3.style.backgroundColor = colorRGB();
+li4.style.backgroundColor = colorRGB();
+li5.style.backgroundColor = colorRGB();
+li6.style.backgroundColor = colorRGB();
+
+//introducimos dentro de ul todos los lis
 const ul = document.createElement("ul");
 ul.append(li1, li2, li3, li4, li5, li6);
 secondMain.append(ul);
+
+//creamos contadores en el footer!
+
+document.body;
+const section1 = document.createElement("section");
