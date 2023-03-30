68bcf8fcd68e4c19d72c9af014cd170125bff3a4
julianalz <julian942@hotmail.com>
2023-03-30 19:55:40 +0200

    actualizacion css

--- javaScr/main.js
+++ javaScr/main.js
@@ -15,10 +15,13 @@ function colorRGB() {
 }
 
 const secondHeader = document.createElement("header");
+const h2 = document.createElement("h2");
 const main = document.querySelector("main");
 const bigheader = document.querySelector("header");
 main.appendChild(secondHeader);
-
+bigheader.appendChild(h2);
+h2.textContent =
+  "¿Quieres jugar? Me ganas la partida si adivinas 3 colores y pierdes si fallas 3 veces..";
 const secondMain = document.createElement("main");
 main.append(secondMain);
 
@@ -29,13 +32,13 @@ bigheader.classList.add("header1");
 
 function reiniciarJuego() {
   secondHeader.textContent = colorRGB();
-  intentos.textContent = "Intentos: 0";
+  fallos.textContent = "Fallos: 0";
   aciertos.textContent = `Aciertos: 0`;
   loser = 0;
   intentosRest = 3;
   crearLis();
 }
-
+let aciertoRest = 3;
 let intentosRest = 3;
 let loser = 0;
 let win = 0;
@@ -48,6 +51,7 @@ function crearLis() {
   const posicionGanadora = generarNumero(5);
   ul.innerHTML = "";
   console.log(posicionGanadora);
+
   for (let i = 0; i <= 5; i++) {
     const a = document.createElement("li");
     if (i === posicionGanadora) {
@@ -63,20 +67,20 @@ function crearLis() {
       console.log(a.style.background, colorParaAdivinar2);
       if (a.style.background === colorParaAdivinar2) {
         win++;
-        aciertos.textContent = `aciertos ${win}`;
-        secondHeader.textContent = "Puto crack";
+        aciertoRest++;
+        aciertos.textContent = `Aciertos ${win}`;
         if (win === 3) {
           secondHeader.textContent = ` Ganaste por ${win} aciertos`;
-          setTimeout(() => {
-            reiniciarJuego();
-          }, 5000);
+          const pantallaGanadora = document.createElement("div");
+          pantallaGanadora.classList.add("winScreen");
+          secondHeader.appendChild(pantallaGanadora);
         } else {
           crearLis();
         }
       } else {
         loser++;
         intentosRest--;
-        intentos.textContent = `Fallos ${loser}`;
+        fallos.textContent = `Fallos ${loser}`;
         secondHeader.textContent = `Fumado compra unas gafas y tienes otros ${intentosRest} intentos`;
         if (loser === 3) {
           secondHeader.textContent = ` Perdiste por ${loser} fallos`;
@@ -98,13 +102,13 @@ crearLis();
 secondMain.append(ul);
 
 const footer = document.createElement("footer");
-const intentos = document.createElement("div");
-intentos.id = "fallos";
-intentos.textContent = "Intentos: 0";
+const fallos = document.createElement("div");
+fallos.id = "fallos";
+fallos.textContent = "Fallos: 0";
 const aciertos = document.createElement("div");
 aciertos.textContent = "Aciertos: 0";
 aciertos.id = "aciertos";
-footer.appendChild(intentos);
+footer.appendChild(fallos);
 footer.appendChild(aciertos);
 
 document.body.appendChild(footer);
